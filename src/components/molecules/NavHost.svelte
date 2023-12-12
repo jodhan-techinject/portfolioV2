06ddcf647df5fac90cea51f339e74a04ca36f580
Jodhansajifab <jodhansaji@gmail.com>
2023-12-12 17:53:52 +0530

    NavBar Changes

--- src/components/molecules/NavHost.svelte
+++ src/components/molecules/NavHost.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Nav from '../atoms/Nav.svelte';
+
 	let y: number;
 </script>
 
@@ -16,59 +17,61 @@
 <svelte:window bind:scrollY={y} />
 
 <style lang="scss">
-	nav {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-	}
-	div {
-		width: 100%;
-		text-align: center;
-		align-items: center;
-		position: fixed;
-		top: 0;
-		padding: 1.25rem 0;
-		z-index: 15;
-		background-color: (var(--bg));
-		backdrop-filter: blur(15px);
-		-webkit-backdrop-filter: blur(15px);
-		background-blend-mode: overlay;
-		transition: all 0.5s ease;
+  nav {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 
-		ul {
-			display: flex;
-			gap: 4rem;
-			justify-content: center;
-		}
-	}
-	@media (min-width: 868px) {
-		div {
-			border-radius: 0;
-			width: 100%;
-		}
-		.scrolled {
-			border-radius: 1000px;
-			border-bottom: 1.5px solid var(--yellow-opacity);
-			padding: 0.5rem 0;
-			margin-top: 0.5rem;
-			width: 34rem;
-			background-color: var(--neutral-five);
-			box-shadow: 0px 15px 8px -10px rgba(0, 0, 0, 0.4);
-		}
-	}
+  div {
+    width: 100%;
+    text-align: center;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    padding: 1.25rem 0;
+    z-index: 15;
+    background-color: (var(--bg));
+    backdrop-filter: blur(15px);
+    -webkit-backdrop-filter: blur(15px);
+    background-blend-mode: overlay;
+    transition: all 0.5s ease;
 
-	@media (max-width: 868px) {
-		div {
-			bottom: 0;
-			top: auto;
-			padding: 0rem;
-			background-color: #1a1616;
-		}
+    ul {
+      display: flex;
+      gap: 4rem;
+      justify-content: center;
+    }
+  }
 
-		div ul {
-			display: flex;
-			justify-content: space-evenly;
-			gap: 2vw;
-		}
-	}
+  @media (min-width: 868px) {
+    div {
+      border-radius: 0;
+      width: 100%;
+    }
+    .scrolled {
+      border-radius: 500px;
+      border-bottom: 1.5px solid var(--yellow-opacity);
+      padding: 0.5rem 0;
+      margin-top: 0.5rem;
+      width: 37rem;
+      background-color: var(--neutral-five);
+      box-shadow: 0px 15px 8px -10px rgba(0, 0, 0, 0.4);
+    }
+  }
+
+  @media (max-width: 868px) {
+    div {
+      bottom: 0;
+      top: auto;
+      padding: 0rem;
+      background-color: #1a1616;
+    }
+
+    div ul {
+      display: flex;
+      justify-content: space-evenly;
+      gap: 2vw;
+    }
+  }
 </style>
--- src/components/organisms/Hero.svelte
+++ src/components/organisms/Hero.svelte
@@ -8,6 +8,7 @@
 		if (!el) return;
 		el.scrollIntoView(true);
 	}
+
 </script>
 
 <section id="home" class="wrapper ">
@@ -23,57 +24,57 @@
 </section>
 
 <style lang="scss">
-	@import '../../styles/mixins.scss';
+  @import '../../styles/mixins.scss';
 
-	section {
-		scroll-margin-top: 20rem;
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		margin-bottom: 5.75rem;
-		margin-top: 7rem;
+  section {
+    scroll-margin-top: 20rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 5.75rem;
+    margin-top: 7rem;
 
-		@media screen and (max-width: 868px) {
-			margin-top: 4rem;
-		}
+    @media screen and (max-width: 868px) {
+      margin-top: 4rem;
+    }
 
-		@media screen and (max-width: 768px) {
-			margin-bottom: 2.75rem;
-		}
+    @media screen and (max-width: 768px) {
+      margin-bottom: 2.75rem;
+    }
 
-		@media screen and (max-width: 500px) {
-			display: block;
-		}
-	}
+    @media screen and (max-width: 500px) {
+      display: block;
+    }
+  }
 
-	.socials {
-		margin-top: 1rem;
-		margin-bottom: 1.7rem;
-	}
+  .socials {
+    margin-top: 1rem;
+    margin-bottom: 1.7rem;
+  }
 
-	h4 {
-		margin-top: 1rem;
-	}
+  h4 {
+    margin-top: 1rem;
+  }
 
-	h1::before {
-		@include outlineText($content: '///', $translateX: -95%, $letterSpacing: -0.1em);
-	}
+  h1::before {
+    @include outlineText($content: '///', $translateX: -95%, $letterSpacing: -0.1em);
+  }
 
-	@media screen and (max-width: 1240px) {
-		h1 {
-			min-height: 0vw;
-			font-size: clamp(4rem, 10vw, 6.25rem);
-		}
+  @media screen and (max-width: 1240px) {
+    h1 {
+      min-height: 0vw;
+      font-size: clamp(4rem, 10vw, 6.25rem);
+    }
 
-		h4 {
-			min-height: 0vw;
-			font-size: clamp(1.4rem, 2.4vw, 1.5rem);
-		}
-	}
+    h4 {
+      min-height: 0vw;
+      font-size: clamp(1.4rem, 2.4vw, 1.5rem);
+    }
+  }
 
-	@media screen and (max-width: 400px) {
-		h4 br {
-			content: '&#160;';
-		}
-	}
+  @media screen and (max-width: 400px) {
+    h4 br {
+      content: '&#160;';
+    }
+  }
 </style>
