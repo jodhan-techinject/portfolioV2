f29d85a52da17b247123f198c090fd7a0860b01c
Jodhansajifab <jodhansaji@gmail.com>
2024-05-31 15:54:26 +0530

    projects updated

--- src/components/molecules/WebGrid.svelte
+++ src/components/molecules/WebGrid.svelte
@@ -4,26 +4,16 @@
 
 <div class="grid-container">
 	<div class="grid">
-		<Website tall technologies="React Native, Node.js, Express.js, postgresql" website="pill-up"
-						 websiteLink="https://www.pillup.com/"
-						 websiteTitle="Pill Up" />
-		<Website technologies="Svelte Kit, BootStrap, Firebase" website="not-so-white" websiteLink="https://notsowhite.in/"
-						 websiteTitle="Not So White" />
-		<Website tall website="lovely-marbles"
-						 websiteLink="https://www.lovelymarblessanitations.com/" websiteTitle="Lovely Marbles And Sanitation" />
-		<Website website="shrey-india" websiteLink="https://www.shreysports.in/"  websiteTitle="Shrey India" />
-		<Website tall website="diverge" />
-		<Website tall website="FADE" />
-		<Website
-			commission
-			shrink
-			subtitle="Commissioned by Kevin T. · Do not use"
-			tall
-			website="flandre"
-		/>
-		<Website tall website="ribbons" />
-		<Website website="asumi" />
-		<Website website="doodle" />
+		<Website tall technologies="React Native, Node.js, Express.js, postgresql" website="pill-up" websiteLink="https://www.pillup.com/" websiteTitle="Pill Up" />
+		<Website website="ikon-living" websiteLink="https://www.ikonliving.in/" websiteTitle="Ikon Living" technologies="Svelte Kit, Tailwind" />
+		<Website tall website="tech-inject" websiteLink="https://www.techinject.in/" websiteTitle="Tech Inject" technologies="Svelte Kit, Bootstrap, JQuery"/>
+		<Website website="careplanet" websiteLink="https://www.careplanetlifesciences.com/" websiteTitle="Care Planet Life Sciences" technologies="HTML, CSS, JS"/>
+		<Website tall website="vitaminx" websiteLink="https://www.vitaminx.in/" websiteTitle="Vitamin X" technologies="Firebase, Svelte Kit, Tailwind"/>
+		<Website tall website="pradeep" websiteTitle="Pradeep Publications" websiteLink="https://pradeep-client.vercel.app/" technologies="Firebase, Svelte Kit, Tailwind"/>
+		<Website tall website="lovely-marbles" websiteLink="https://www.lovelymarblessanitations.com/" websiteTitle="Lovely Marbles And Sanitation" technologies="React , Firebase, Tailwind" />
+		<Website tall website="shrey-india" websiteLink="https://www.shreysports.in/"  websiteTitle="Shrey India" technologies="Firebase, Svelte Kit, Tailwind"/>
+		<Website website="neualto" websiteLink="https://www.neualto.com/" websiteTitle="Neualto" technologies="Firebase, Svelte Kit, Tailwind"/>
+		<Website technologies="Svelte Kit, BootStrap, Firebase" website="not-so-white" websiteLink="https://notsowhite.in/" websiteTitle="Not So White" />
 	</div>
 </div>
 
