11d7347252ac013d88abab33b358690b29adbfaf
jodhan-techinject <jodhansaji@gmail.com>
2024-10-16 23:33:00 +0530

    minor changes

--- src/components/organisms/Hero.svelte
+++ src/components/organisms/Hero.svelte
@@ -14,7 +14,7 @@
 <section id="home" class="wrapper ">
 	<div>
 		<h1>Jodhan</h1>
-		<h4>Web Alchemist. <br /> Full Stack.</h4>
+		<h4>Web Dev. <br /> Full Stack.</h4>
 		<div class="socials">
 			<Socials />
 		</div>
